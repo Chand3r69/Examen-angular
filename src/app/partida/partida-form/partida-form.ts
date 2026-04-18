6d60432feb8d2f63b366b7d6271ce3ba8709b847
Jhair Anderson Choque <Jhair69tesla@gmail.com>
2026-04-18 16:57:00 -0400

    Avance de Partida

--- src/app/partida/partida-form/partida-form.ts
+++ src/app/partida/partida-form/partida-form.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Partida } from '../../modelos/partida';
+import { Jugador } from '../../modelos/jugador';
+import { PartidaService } from '../../servicios/partida';
+import { JugadorService } from '../../servicios/jugador';
 
 @Component({
   selector: 'app-partida-form',
-  imports: [],
+  imports: [FormsModule,CommonModule],
   templateUrl: './partida-form.html',
   styleUrl: './partida-form.css',
 })
-export class PartidaForm {}
+export class PartidaForm {
+  jugadores= signal<Jugador[]>([]);
+  partida = signal<Partida>({
+    puntuacion: 0,
+    duracion: 0,
+    fecha: '',
+    jugadorId: 0
+  });
+
+  constructor(
+    private jugadorService: JugadorService,
+    private partidaService: PartidaService
+  ) {
+    this.cargarJugadores();
+  }
+  async cargarJugadores() {
+    this.jugadores.set(await this.jugadorService.listar());
+  }
+
+  async guardar() {
+    await this.partidaService.crear(this.partida());
+    alert('Partida creada');
+  }
+
+}
--- src/app/partida/partida-list/partida-list.ts
+++ src/app/partida/partida-list/partida-list.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PartidaService } from '../../servicios/partida';
+import { JugadorService } from '../../servicios/jugador';
+import { Partida } from '../../modelos/partida';
+import { Jugador } from '../../modelos/jugador';
+
 
 @Component({
   selector: 'app-partida-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './partida-list.html',
   styleUrl: './partida-list.css',
 })
-export class PartidaList {}
+export class PartidaList {
+  partidas = signal<Partida[]>([]);
+  jugadores = signal<Jugador[]>([]);
+
+  constructor(
+    private partidaService: PartidaService,
+    private jugadorService: JugadorService
+  ) {
+    this.cargar();
+  }
+
+  async cargar() {
+    this.partidas.set(await this.partidaService.listar());
+    this.jugadores.set(await this.jugadorService.listar());
+  }
+
+  getJugadorNombre(id: number) {
+    return this.jugadores().find(j => j.id === id)?.nickname || 'N/A';
+  }
+
+}
